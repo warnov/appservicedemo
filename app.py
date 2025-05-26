4d9d9d26529e946affb8c32aabe309fb8c9e1704
warnov <warnov@gmail.com>
2025-05-26 12:29:33 -0500

    solved dependencies

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return '<h1>¡Hola, Azure App Service con Python! 11:59</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80).ve 

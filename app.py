4741533151f83abdc6b66020841d7c513958f273
warnov <warnov@gmail.com>
2025-05-26 12:08:09 -0500

    live update1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>¡Hola, Azure App Service con Python! 11:58</h1>'
+    return '<h1>¡Hola, Azure App Service con Python! 11:59</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)

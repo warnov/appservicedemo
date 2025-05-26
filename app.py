feabebc722529c9b50ee9fe724d14d25eede5740
warnov <warnov@gmail.com>
2025-05-26 12:56:15 -0500

    v5

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return '<h1>¡Hola, Azure App Service con Python! 12:00</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80).ve 
+    app.run(host='0.0.0.0', port=80)
